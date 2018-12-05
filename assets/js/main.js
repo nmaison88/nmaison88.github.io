dd5625377117dd2aca5ff71b2e70a825db5f19af
Neth Maison <monumentcalifornia@gmail.com>
2018-12-05 07:25:19 -0800

    Updated Index.html and added new portfolio items

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,105 @@
-"use strict";
-jQuery( document ).ready( function ( $ ) {
+/*
+	Overflow by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-//for Preloader
+(function($) {
 
-    $( window ).load( function () {
-        $( "#loading" ).fadeOut( 500 );
-    } );
+	var	$window = $(window),
+		$body = $('body'),
+		settings = {
 
+			// Parallax background effect?
+				parallax: true,
 
-    /*---------------------------------------------*
-     * Mobile menu
-     ---------------------------------------------*/
-    $( '.link' ).find( 'a[href*=#]:not([href=#])' ).click( function () {
-        if ( location.pathname.replace( /^\//, '' ) == this.pathname.replace( /^\//, '' ) && location.hostname == this.hostname ) {
-            var target = $( this.hash );
-            target = target.length ? target : $( '[name=' + this.hash.slice( 1 ) + ']' );
-            if ( target.length ) {
-                $( 'html,body' ).animate( {
-                    scrollTop: ( target.offset().top - 80 )
-                }, 1000 );
-                if ( $( '.navbar-toggle' ).css( 'display' ) != 'none' ) {
-                    $( this ).parents( '.container' ).find( ".navbar-toggle" ).trigger( "click" );
-                }
-                return false;
-            }
-        }
-    } );
+			// Parallax factor (lower = more intense, higher = less intense).
+				parallaxFactor: 10
 
+		};
 
+	// Breakpoints.
+		breakpoints({
+			wide:    [ '1081px',  '1680px' ],
+			normal:  [ '841px',   '1080px' ],
+			narrow:  [ '737px',   '840px'  ],
+			mobile:  [ null,      '736px'  ]
+		});
 
-    /*---------------------------------------------*
-     * WOW
-     ---------------------------------------------*/
+	// Mobile?
+		if (browser.mobile)
+			$body.addClass('is-scroll');
 
-    var wow = new WOW( {
-        mobile: false // trigger animations on mobile devices (default is true)
-    } );
-    wow.init();
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-// magnificPopup
+	// Scrolly.
+		$('.scrolly-middle').scrolly({
+			speed: 1000,
+			anchor: 'middle'
+		});
 
-    $( '.popup-img' ).magnificPopup( {
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    } );
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() { return (breakpoints.active('<=mobile') ? 70 : 190); }
+		});
 
-    $( '.video-link' ).magnificPopup( {
-        type: 'iframe'
-    } );
+	// Parallax background.
 
+		// Disable parallax on IE/Edge (smooth scrolling is jerky), and on mobile platforms (= better performance).
+			if (browser.name == 'ie'
+			||	browser.name == 'edge'
+			||	browser.mobile)
+				settings.parallax = false;
 
-    $( '.mouse-scroll' ).bind( 'click', function () {
-        $( 'html , body' ).stop().animate( {
-            scrollTop: $( $( this ).attr( 'href' ) ).offset().top - 60
-        }, 1500, 'easeInOutExpo' );
-        event.preventDefault();
-    } );
+		if (settings.parallax) {
 
+			var $dummy = $(), $bg;
 
-} );
+			$window
+				.on('scroll.overflow_parallax', function() {
 
+					// Adjust background position.
+						$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
 
+				})
+				.on('resize.overflow_parallax', function() {
 
+					// If we're in a situation where we need to temporarily disable parallax, do so.
+						if (breakpoints.active('<=narrow')) {
 
+							$body.css('background-position', '');
+							$bg = $dummy;
 
+						}
 
+					// Otherwise, continue as normal.
+						else
+							$bg = $body;
 
+					// Trigger scroll handler.
+						$window.triggerHandler('scroll.overflow_parallax');
 
+				})
+				.trigger('resize.overflow_parallax');
 
+		}
 
+	// Poptrox.
+		$('.gallery').poptrox({
+			useBodyOverflow: false,
+			usePopupEasyClose: false,
+			overlayColor: '#0a1919',
+			overlayOpacity: 0.75,
+			usePopupDefaultStyling: false,
+			usePopupCaption: true,
+			popupLoaderText: '',
+			windowMargin: 10,
+			usePopupNav: true
+		});
 
-
-
+})(jQuery);
